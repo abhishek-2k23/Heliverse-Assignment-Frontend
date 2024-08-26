a1c64cb9a99e44660366fdab237af277228fb6e5
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-26 19:10:31 +0530

    redux implemented

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,51 +1,60 @@
-import React, { useState, useEffect, useRef, useCallback } from "react"
-import { Link } from "react-router-dom"
-import { CiMenuKebab } from "react-icons/ci"
-import { BsSearch } from "react-icons/bs"
-import { FaAnglesDown } from "react-icons/fa6"
-import { IoCloseSharp } from "react-icons/io5"
+import { useState, useEffect, useRef, useCallback } from "react";
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { FaAnglesDown } from "react-icons/fa6";
+import { IoCloseSharp } from "react-icons/io5";
 import { RiMenu3Line } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setSearchTerm,
+  setFilterOption,
+  setShowFilterMenu,
+  setShowSliderMenu,
+} from "../redux_store/slices/nav.slice";
 
 const Nav = () => {
-  const [searchTerm, setSearchTerm] = useState("")
-  const [filterOption, setFilterOption] = useState("")
-  const [showFilterMenu, setShowFilterMenu] = useState(false)
-  const [showSliderMenu, setShowSliderMenu] = useState(false)
+  const navStates = useSelector((store) => store.nav);
+  const {searchTerm,
+    showSliderMenu,
+    showFilterMenu,
+    filterOption,
+  } = navStates;
+  const dispatch = useDispatch();
 
-  const filterMenuRef = useRef(null)
-  const sliderMenuRef = useRef(null)
+  const filterMenuRef = useRef(null);
+  const sliderMenuRef = useRef(null);
 
   // Debounce function
   const debounce = (func, delay) => {
-    let debounceTimer
+    let debounceTimer;
     return (...args) => {
-      clearTimeout(debounceTimer)
-      debounceTimer = setTimeout(() => func.apply(null, args), delay)
-    }
-  }
+      clearTimeout(debounceTimer);
+      debounceTimer = setTimeout(() => func.apply(null, args), delay);
+    };
+  };
 
   // API call for search with debouncing
   const handleSearch = useCallback(
     debounce((term) => {
-      console.log(`Searching for: ${term}`)
+      console.log(`Searching for: ${term}`);
       // API call logic here
     }, 300),
-    [],
-  )
+    []
+  );
 
   // Handle input change for search
   useEffect(() => {
     if (searchTerm) {
-      handleSearch(searchTerm)
+      handleSearch(searchTerm);
     }
-  }, [searchTerm, handleSearch])
+  }, [searchTerm, handleSearch]);
 
   const handleFilterSelect = (option) => {
-    setFilterOption(option)
-    setShowFilterMenu(false)
-    console.log(`Filtering by: ${option}`)
+    dispatch(setFilterOption(option));
+    dispatch(setShowFilterMenu(false));
+    console.log(`Filtering by: ${option}`);
     // API call logic here
-  }
+  };
 
   // Close menus when clicking outside
   useEffect(() => {
@@ -54,21 +63,21 @@ const Nav = () => {
         filterMenuRef.current &&
         !filterMenuRef.current.contains(event.target)
       ) {
-        setShowFilterMenu(false)
+        dispatch(setShowFilterMenu(false));
       }
       if (
         sliderMenuRef.current &&
         !sliderMenuRef.current.contains(event.target)
       ) {
-        setShowSliderMenu(false)
+        dispatch(setShowSliderMenu(false));
       }
-    }
+    };
 
-    document.addEventListener("mousedown", handleClickOutside)
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [])
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dispatch]);
 
   return (
     <nav className="bg-gray-800 p-4">
@@ -85,7 +94,7 @@ const Nav = () => {
               type="text"
               placeholder="Search"
               value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
+              onChange={(e) => dispatch(setSearchTerm(e.target.value))}
               className="px-3 py-2 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
             <div className="absolute bottom-3 right-3">
@@ -95,9 +104,9 @@ const Nav = () => {
           <div className="relative" ref={filterMenuRef}>
             <div
               className="flex justify-between items-center px-2 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-900-500 text-gray-700 bg-white space-x-5 w-36"
-              onClick={() => setShowFilterMenu(!showFilterMenu)}
+              onClick={() => dispatch(setShowFilterMenu(!showFilterMenu))}
             >
-              <button className="font-semibold tracking-wider">Filter</button>
+              <button className="font-semibold tracking-wider">{filterOption}</button>
               <FaAnglesDown />
             </div>
 
@@ -119,8 +128,11 @@ const Nav = () => {
             )}
           </div>
         </div>
-        <div className="md:hidden text-white font-extrabold text-xl hover:text-gray-400 cursor-pointer" onClick={() => setShowSliderMenu(!showSliderMenu)}>
-            <RiMenu3Line />
+        <div
+          className="md:hidden text-white font-extrabold text-xl hover:text-gray-400 cursor-pointer"
+          onClick={() => dispatch(setShowSliderMenu(!showSliderMenu))}
+        >
+          <RiMenu3Line />
         </div>
       </div>
       {/* Slider Menu for small screens */}
@@ -130,16 +142,18 @@ const Nav = () => {
           className="md:hidden fixed top-0 left-0 w-64 h-full bg-gray-800 text-white p-4 z-50"
         >
           <div className="flex justify-end ">
-            <div className="flex justify-center items-center text-gray-800 bg-white w-7 h-7 rounded-full cursor-pointer" onClick={() => setShowSliderMenu(false)}>
-              
-                <IoCloseSharp />
+            <div
+              className="flex justify-center items-center text-gray-800 bg-white w-7 h-7 rounded-full cursor-pointer"
+              onClick={() => dispatch(setShowSliderMenu(!showSliderMenu))}
+            >
+              <IoCloseSharp />
             </div>
           </div>
           <div className="mt-4 space-y-4">
             <Link
               to="/team"
               className="block hover:text-gray-400"
-              onClick={() => setShowSliderMenu(false)}
+              onClick={() => dispatch(setShowSliderMenu(!showSliderMenu))}
             >
               Team
             </Link>
@@ -148,16 +162,16 @@ const Nav = () => {
                 type="text"
                 placeholder="Search"
                 value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
+                onChange={(e) => dispatch(setSearchTerm(e.target.value))}
                 className="w-full px-3 py-2 text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
               />
             </div>
             <div className="relative" ref={filterMenuRef}>
               <div
                 className="flex justify-between items-center px-2 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-900-500 text-gray-700 bg-white space-x-5 w-full"
-                onClick={() => setShowFilterMenu(!showFilterMenu)}
+                onClick={() => dispatch(setShowFilterMenu(!showFilterMenu))}
               >
-                <button className="font-semibold tracking-wider">Filter</button>
+                <button className="font-semibold tracking-wider">{filterOption}</button>
                 <FaAnglesDown />
               </div>
 
@@ -182,7 +196,7 @@ const Nav = () => {
         </div>
       )}
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;

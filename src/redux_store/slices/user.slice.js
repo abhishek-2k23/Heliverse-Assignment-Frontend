3ff7d1264295fc7c2571df9ce15584ed541f49e1
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-27 19:46:44 +0530

    user related data  added

--- src/redux_store/slices/user.slice.js
+++ src/redux_store/slices/user.slice.js
@@ -3,17 +3,21 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
   name: 'users',
   initialState: {
-    data: [],
     currentPage: 1,
     totalPages: 1,
     loading: true,
     error: '',
     isEditing: false,
+    editUserId: null,
+    refreshData: false,
+    domain_list: [],
+    data: [],
   },
   reducers: {
     setUsers(state, action) {
       state.data = action.payload;
-      console.log("user Data : ", state.data);
+      console.log("user Data : ", state);
+
     },
     setCurrentPage(state, action) {
         state.currentPage = action.payload;
@@ -23,10 +27,23 @@ const userSlice = createSlice({
     },
     setIsEditing: (state,action) => {
       state.isEditing = action.payload;
+    },
+    setTotalPages: (state, action) => {
+      state.totalPages = action.payload;
+      console.log("total pages : ", state.totalPages)
+    },
+    setDomainList: (state, action) => {
+      state.domain_list = action.payload;
+    },
+    setEditUserId: (state,action) => {
+      state.editUserId = action.payload;
+    },
+    setRefreshData: (state,action) => {
+      state.refreshData = action.payload;
     }
   },
 });
 
-export const { setUsers, updateUser, deleteUser, setCurrentPage, setIsEditing } = userSlice.actions;
+export const { setUsers, updateUser, deleteUser, setCurrentPage, setIsEditing,setTotalPages,setDomainList,setEditUserId,setRefreshData } = userSlice.actions;
 
 export default userSlice.reducer;

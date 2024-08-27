b9321c3e3217656d53af86c8412ffbd29e6a5632
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 01:04:34 +0530

    filterCategory component added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import UserList from '../components/UsersList';
 import Pagination from '../components/Pagination';
+import { useSelector } from 'react-redux';
+import ShowFilterOptions from '../components/ShowFilterOptions';
+
 const Home = () => {
+  const filterOption = useSelector((store) => store.nav.filterOption);
+
   return (
     <div className='min-h-screen'>
+      {
+        filterOption !== 'Filter' && <ShowFilterOptions />
+      }
       <UserList />
       <Pagination />
     </div>

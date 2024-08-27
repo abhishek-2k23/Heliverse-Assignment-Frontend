37a4aa654d6acbf2d5b04bfe86c906791e36c6fe
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-27 19:47:13 +0530

    bg changed and pagination component added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import UserList from '../components/UsersList';
-
+import Pagination from '../components/Pagination';
 const Home = () => {
   return (
-    <>
+    <div className='min-h-screen'>
       <UserList />
-    </>
+      <Pagination />
+    </div>
   )
 }
 

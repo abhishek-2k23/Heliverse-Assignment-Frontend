fafdd7a9a46a37c5f6fdaf08d0cda051561df8b2
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-27 19:47:46 +0530

    custom hooks used and bugs fixed

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,10 +1,11 @@
 // src/components/UserList.js
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setUsers, deleteUser } from '../redux_store/slices/user.slice';
+import { setUsers, deleteUser, setDomainList, setTotalPages, setCurrentPage } from '../redux_store/slices/user.slice';
 import axios from 'axios';
 const api_url = import.meta.env.VITE_API_URL;
 import UsersCard from '../components/UserCard';
+import { useFetchUser } from '../hooks/useFetchUser';
 
 const UserList = () => {
   const dispatch = useDispatch();
@@ -12,54 +13,15 @@ const UserList = () => {
   const [editingUserId, setEditingUserId] = useState(null);
   const [formData, setFormData] = useState({});
 
-  
-
-
-  const fetchUser = async() => {
-    try{
-      const userData = await axios.get(`${api_url}/users?page=${1}&limit=20`);
-      console.log("fetched user data : ", userData);
-
-      //set data to the redux
-      dispatch(setUsers(userData?.data?.users))
-    }catch(e){
-      console.error(e.message);
-    }
-  }
-  useEffect(() => {
-    fetchUser();
-  },[]);
-
-  const handleUpdate = (id, updatedData) => {
-    axios.put(`/api/users/${id}`, updatedData)
-      .then(response => {
-        const updatedUsers = users.map(user =>
-          user.id === id ? { ...user, ...updatedData } : user
-        );
-        setUsers(updatedUsers);
-      })
-      .catch(error => console.error(error));
-  };
-
-  const handleDelete = (id) => {
-    axios.delete(`/api/users/${id}`)
-      .then(() => {
-        const filteredUsers = users.filter(user => user.id !== id);
-        setUsers(filteredUsers);
-      })
-      .catch(error => console.error(error));
-  };
+  useFetchUser();
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4 bg-gray-900 min-h-screen">
       {users?.map(user => (
         <UsersCard
           key={user.id}
           user={user}
           id={user?.id}
-          fetchUser={fetchUser}
-          onUpdate={handleUpdate}
-          onDelete={handleDelete}
         />
       ))}
     </div>

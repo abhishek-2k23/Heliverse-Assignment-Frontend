09f4e84d604aac8567d7ffb89556336d2c779922
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 14:06:27 +0530

    hot taost config

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import {Provider} from 'react-redux';
 import { store } from './redux_store/app.store.js'
-
+import {Toaster} from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster />
       <RouterProvider router={AppRouter} />
     </Provider>
   </StrictMode>

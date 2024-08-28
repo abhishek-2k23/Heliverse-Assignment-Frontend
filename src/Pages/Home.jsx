7f114d933a3ca20c746b7d5bb3547d5fa1636d8a
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 22:55:27 +0530

    code refactor and loading condition

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,35 @@
-import UserList from '../components/UsersList';
-import Pagination from '../components/Pagination';
-import { useSelector, useDispatch } from 'react-redux';
-import ShowFilterOptions from '../components/ShowFilterOptions';
-import TeamList from '../components/TeamList';
-import { setShowTeamView } from '../redux_store/slices/team.slice';
-import AddUser from '../components/AddUser';
+import UserList from "../components/UsersList"
+import Pagination from "../components/Pagination"
+import { useSelector } from "react-redux"
+import ShowFilterOptions from "../components/ShowFilterOptions"
+import TeamList from "../components/TeamList"
+import AddUser from "../components/AddUser"
 
 const Home = () => {
-  const filterOption = useSelector((store) => store.nav.filterOption);
-  const addUser = useSelector((store) => store.nav.addUser);
-  const show_team_view = useSelector((store) => store.team.show_team_view);
+  const filterOption = useSelector((store) => store.nav.filterOption)
+  const addUser = useSelector((store) => store.nav.addUser)
+  const show_team_view = useSelector((store) => store.team.show_team_view)
+  const loading = useSelector((store) => store.users.loading)
 
   return (
     <div className="min-h-screen">
-      {filterOption !== 'Filter' && <ShowFilterOptions />}
-      {addUser && <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm bg-black/30"> <AddUser /> </div>}
+      {filterOption !== "Filter" && <ShowFilterOptions />}
+      {addUser && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm bg-black/30">
+          {" "}
+          <AddUser />{" "}
+        </div>
+      )}
       {show_team_view && (
         <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm bg-black/30">
-          <TeamList  />
+          <TeamList />
         </div>
       )}
       <UserList />
-      <Pagination />
-    </div>
-  );
-};
 
-export default Home;
+      {!loading && <Pagination />}
+    </div>
+  )
+}
+
+export default Home

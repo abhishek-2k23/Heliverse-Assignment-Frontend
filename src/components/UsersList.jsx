ef365db4707edc145612f91068b71b40fdb47a84
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 22:53:31 +0530

    loading condition

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,25 +1,36 @@
-import { useSelector } from 'react-redux';
-import UsersCard from '../components/UserCard';
-import { useFetchUser } from '../hooks/useFetchUser';
+import { useDispatch, useSelector } from "react-redux"
+import UsersCard from "../components/UserCard"
+import { useFetchUser } from "../hooks/useFetchUser"
+import Loader from "./Loader"
+import { setAddUser } from "../redux_store/slices/nav.slice"
 
 const UserList = () => {
-  // const dispatch = useDispatch();
-  const users = useSelector((state) => state.users.data);
-  // const [editingUserId, setEditingUserId] = useState(null);
-  // const [formData, setFormData] = useState({});
+  const users = useSelector((state) => state.users.data)
+  const loading = useSelector((state) => state.users.loading)
+  const dispatch = useDispatch()
 
-  useFetchUser();
+  useFetchUser()
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 p-4 bg-gray-900 min-h-screen">
-      {users?.map(user => (
-        <UsersCard
-          key={user.id}
-          user={user}
-          id={user?.id}
-        />
-      ))}
+      {loading ? (
+        <Loader />
+      ) : (!users || users === "undefined" )? (
+        <div className="absolute top-1/2 left-[45%] text-white text-xl cursor-pointer">
+          No User Available.{" "}
+          <span
+            className="italic underline tracking-wide"
+            onClick={() => dispatch(setAddUser(true))}
+          >
+            Create user
+          </span>
+        </div>
+      ) : (
+        users?.map((user) => (
+          <UsersCard key={user.id} user={user} id={user?.id} />
+        ))
+      )}
     </div>
-  );
-};
-export default UserList;
+  )
+}
+export default UserList

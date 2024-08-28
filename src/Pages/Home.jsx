2201fc106116d8b0ce9a49987143b83703d215c4
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-28 14:08:21 +0530

    option team section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
 import UserList from '../components/UsersList';
 import Pagination from '../components/Pagination';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ShowFilterOptions from '../components/ShowFilterOptions';
+import TeamList from '../components/TeamList';
+import { setShowTeamView } from '../redux_store/slices/team.slice';
 
 const Home = () => {
   const filterOption = useSelector((store) => store.nav.filterOption);
+  const show_team_view = useSelector((store) => store.team.show_team_view);
+  const dispatch = useDispatch();
 
   return (
-    <div className='min-h-screen'>
-      {
-        filterOption !== 'Filter' && <ShowFilterOptions />
-      }
+    <div className="min-h-screen">
+      {filterOption !== 'Filter' && <ShowFilterOptions />}
+      {show_team_view && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-blur-sm bg-black/30">
+          <TeamList 
+            onClose={() => dispatch(setShowTeamView(false))} 
+          />
+        </div>
+      )}
       <UserList />
       <Pagination />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
